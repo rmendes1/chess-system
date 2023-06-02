030412e1edbd5a26f8df7cf5ab2218582b963a46
rmendes1 <joaorenatomendes@gmail.com>
2023-06-02 11:28:26 -0300

    Chess layer and printing the board

--- chessSystem/src/application/Main.java
+++ chessSystem/src/application/Main.java
@@ -2,12 +2,15 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
+import chess.ChessPiece;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
