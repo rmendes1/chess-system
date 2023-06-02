88ce08de0d56abba42a73fa63759af049b6e5bd8
rmendes1 <joaorenatomendes@gmail.com>
2023-06-02 10:49:28 -0300

    project directory created; first class added: Position

---
+++ chessSystem/src/application/Main.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Position;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
