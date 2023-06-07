8d2cda4e3afcf9af5892846d93eb15610a461259
rmendes1 <joaorenatomendes@gmail.com>
2023-06-07 10:10:31 -0300

    Implementing Rook's possible moves

--- chessSystem/src/chess/pieces/Rook.java
+++ chessSystem/src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,6 +17,57 @@ public class Rook extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position paux = new Position(0,0);
+
+        // verify if above the piece is possible to move
+        paux.setValues(position.getRow() - 1, position.getColumn());
+        while (getBoard().positionExists(paux) && !getBoard().thereIsAPiece(paux)) {
+            mat[paux.getRow()][paux.getColumn()] = true;
+            paux.setRow(paux.getRow() - 1);
+        }
+
+        if (getBoard().positionExists(paux) && isThereOpponentPiece(paux)) {
+            mat[paux.getRow()][paux.getColumn()] = true;
+        }
+
+        // verify left
+        paux.setValues(position.getRow(), position.getColumn() - 1);
+        while (getBoard().positionExists(paux) && !getBoard().thereIsAPiece(paux)) {
+            mat[paux.getRow()][paux.getColumn()] = true;
+            paux.setColumn(paux.getColumn() - 1);
+        }
+
+        if (getBoard().positionExists(paux) && isThereOpponentPiece(paux)) {
+            mat[paux.getRow()][paux.getColumn()] = true;
+        }
+
+        // verify right
+        paux.setValues(position.getRow(), position.getColumn() + 1);
+        while (getBoard().positionExists(paux) && !getBoard().thereIsAPiece(paux)) {
+            mat[paux.getRow()][paux.getColumn()] = true;
+            paux.setColumn(paux.getColumn() + 1);
+        }
+
+        if (getBoard().positionExists(paux) && isThereOpponentPiece(paux)) {
+            mat[paux.getRow()][paux.getColumn()] = true;
+        }
+
+        // verify below
+
+        paux.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExists(paux) && !getBoard().thereIsAPiece(paux)) {
+            mat[paux.getRow()][paux.getColumn()] = true;
+            paux.setRow(paux.getRow() + 1);
+        }
+
+        if (getBoard().positionExists(paux) && isThereOpponentPiece(paux)) {
+            mat[paux.getRow()][paux.getColumn()] = true;
+        }
+
+
+        return mat;
+
     }
 }
